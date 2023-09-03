90f1c85f20553687f0de285d5e416688e9056a9c
syuzrh27 <syuja27@gmail.com>
2023-09-03 22:00:55 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "03_Login Page_PAPB"
+include(":app")
+ 
